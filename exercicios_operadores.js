5b1cc116fb15450539e15abdf363caec9e0610c9
Cornelio Costa <cornelio.costa@gmail.com>
2022-12-25 00:26:10 -0300

    09 - Tipos de variaveis

---
+++ exercicios_operadores.js
@@ -0,0 +1,15 @@
+//criando variaveis
+a = 1;
+b = 2;
+c = 3;
+d = '3';
+e = false;
+f = true;
+
+//imprimindo na tela os valores das expressões
+console.log(a+b**c - c % 2);
+console.log(b * c/(a-a));
+console.log((c==d)!=e);
+console.log((b>=a) == f);
+console.log(c != d) && (b % 1 == 0);
+console.log(e || (b > c || f));
