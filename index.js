225c3d6af3fd3c49700f4258f41d8c10cf3265d4
Cornelio Costa <72445479+Cornelio1983@users.noreply.github.com>
2023-01-14 15:37:37 +0000

    Calcular IMC de uma pessoa

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
-// Algoritmo para receber dez alturas de dez alunos
-// Critérios: exibir para cada aluno quantos são menores que ele
+// Funcao para calcular o IMC de uma pessoa
+// Critérios: a funcao deve receber o peso e a altura de uma pessoa e retornar o IMC
 
-// Chamando a biblioteca prompt-sync
-const prompt = require('prompt-sync')();
+// Criando a funcao para calcular IMC
+function calcular_IMC(peso, altura) {
+    // declarando variavel IMC
+    let IMC;
+    // criando equacao para calcular valor do IMC
+    IMC = peso / (altura * altura);
+    // funcao para retornar o valor do IMC
+    return IMC;
+}
 
-// Declarando variável vetorial para guardar valores das alturas
-var alturas = [];
-// Declarando variavel para guardar menor altura
-var menorAltura = [3];
-// Declarando variável para guardar maior altura 
-var maiorAltura = [];
+// Criando funcao para verificar situacao da pessoa relacionado ao IMC
+function verificar_situacao(IMC) {
+    // declarando variavel auxiliar
+    let situacao;
 
-// Implementar a entrada de dados
-for (var i = 0; i <= 9; i++) {
-    // mostra posicao do aluno dentro do array
-    console.log('Posicao do aluno no vetor Altura: ', i);
-    // solicitando altura do aluno
-    alturaAluno = parseFloat(prompt('Digite a altura do aluno: '));
-    // guardando todas as alturas no vetor alturas
-    alturas.push(alturaAluno);
-    // saltando um linha
-    console.log('\n');
-    // Implementar o processamento de dados
-    for (var j = 0; j <= 9; j++) {
-        // implementando condicao para encontrar menor altura
-        if (alturas[i] < menorAltura) {
-            // guardando a menor altura na variavel menorAltura
-            menorAltura = alturas[i];
-        // implementando condicao pra encontrar maior altura
-        } else if (alturas[i] > maiorAltura) {
-            // guardando maior altura na variavels maiorAltura
-            maiorAltura = alturas[i];
-        }
+    // implementando condicao para verificar situacao
+
+    if (IMC < 18.5) {
+        // se IMC menor que 18.5, retornar situacao 1
+        situacao = 1;
+    } else if (IMC >= 18.5 && IMC <= 24.9) {
+        // se IMC maior que 18.5 e menor que 24.9, retornar situacao 2
+        situacao = 2;
+    } else if (IMC > 24.9 && IMC <= 29.9) {
+        // se IMC maior que 24.99 e menor que 29.9, retornar situacao 3
+        situacao = 3;
+    } else {
+        // se IMC diferente das situações anteriores, retornar situacao 4
+        situacao = 4;
+    }
+    // metodo para retorar situcao encontrada
+    return situacao;
+}
+
+// Funcao (procedimento) para exibir descricao de cada situacao
+function exibir_descricao_da_situacao(situacao) {
+    if (situacao == 1) {
+        console.log('Você está abaixo do peso ideal!');
+    } else if (situacao == 2) {
+        console.log('Parabéns, você em seu peso normal!');
+    } else if (situacao == 3) {
+        console.log('Você está com sobrepeso!');
+    } else {
+        console.log('Obesidade!');
     }
 }
-// imprimindo array colecao de alturas
-console.log('Coleção Array de alturas: ', alturas);
-// mostrando a menor altura
-console.log('A menor altura é: ', menorAltura);
-// mostrando a maior altura
-console.log('A maior altura é: ', maiorAltura);
-// imprimindo um salto de linha
-console.log('\n');
+
+// Chamando bilioteca prompt-sync
+const prompt = require('prompt-sync')();
+
+// Implementando laço para captar dados das pessoa
+for (let i = 1; i < 6; i++) {
+    // imprimindo controle de pessoas
+    console.log('PESSOA ', i);
+    // declarando variaveis para captar dados de cinco pessoas
+    let nome = prompt('Digite seu nome: ');
+    let peso = parseFloat(prompt('Digite seu peso (kg): '));
+    let altura = parseFloat(prompt('Digite sua altura (Metros): '));
+    // declarando variavel IMC par chamar funcao que calcula o seu valor
+    let IMC = calcular_IMC(peso, altura);
+    // declarando variavel local para chamar funcao que verifica situação do IMC
+    let situacao = verificar_situacao(IMC);
+    //console.log(situacao)
+    // chamando funcao para exibir situacao
+    exibir_descricao_da_situacao(situacao);
+    // imprimir salto de linha entre uma pessoa e outra
+    console.log('\n');
+}
